11be408278259b027a38e13dbf6f10fe4dc5656d
blinry <mail@blinry.org>
2024-11-15 14:48:10 +0100

    Clean up unused files

--- _config.ts
+++ _config.ts
@@ -1,12 +1,7 @@
 import lume from "lume/mod.ts";
-import jsx_preact from "lume/plugins/jsx_preact.ts";
-import mdx from "lume/plugins/mdx.ts";
-import minify_html from "lume/plugins/minify_html.ts";
-import pagefind from "lume/plugins/pagefind.ts";
-import postcss from "lume/plugins/postcss.ts";
-import tailwindcss from "lume/plugins/tailwindcss.ts";
 import sass from "lume/plugins/sass.ts";
 import multilanguage from "lume/plugins/multilanguage.ts";
+import googleFonts from "lume/plugins/google_fonts.ts";
 
 const site = lume({
   src: "./content",
@@ -22,16 +17,16 @@ site
       defaultLanguage: "original",
     }),
   )
-  .use(jsx_preact())
-  .use(mdx())
-  .use(pagefind())
   .use(sass())
-  // .use(tailwindcss())
-  .use(postcss())
-  // .use(minify_html())
-  .copy("media")
-  .copy("assets", ".")
-  .copy("main.js")
+  .use(
+    googleFonts({
+      fonts:
+        "https://fonts.google.com/share?selection.family=Montserrat:ital,wght@0,400..900;1,400..900",
+    }),
+  )
+  //.copy("media")
+  //.copy("assets", ".")
+  //.copy("main.js")
   .copy([".jpg", ".png", ".pdf", ".svg", ".mp3", ".zip", ".mp4"])
   .remoteFile(
     "cubetopia/Cubetopia_1.03_Mac.zip",
@@ -80,18 +75,6 @@ site
   .remoteFile(
     "unwahrscheinliche-reise/Hörspiel_Die unwahrscheinliche Reise.mp3",
     `${bigFileServer}/H%C3%B6rspiel_Die%20unwahrscheinliche%20Reise.mp3`,
-  )
-  .remoteFile(
-    "./code.css",
-    "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/github.min.css",
-  )
-  .remoteFile(
-    "./bootstrap.css",
-    "https://esm.sh/bootstrap@5.2.3/dist/css/bootstrap.min.css",
-  )
-  .remoteFile(
-    "./darktheme.css",
-    "https://esm.sh/bootstrap-darkmode@5.0.1/css/darktheme.css",
   );
 
 export default site;
